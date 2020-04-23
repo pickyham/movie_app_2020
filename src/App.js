56fcee0f762f91f79fc7ae73954aa39c73dc3668
pickyham <hammieprogrammer@gmail.com>
2020-04-23 20:54:25 +0900

    6  액션 10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import axios from './Movie';
 
 class App extends React.Component{
 state = {
@@ -11,18 +12,26 @@ getMovies = async() => {
    data :{
      data:{movies},
    },
- } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+ } = await axios.get('https://yts-proxy.now.sh/list_movies. json?sort_by=rating');
 //  console.log(movies);/
 this.setState({movies, isLoading : false});
 }
 componentDidMount(){
 this.getMovies();
 }
-render(){
-  const {isLoading} = this.state;
-  return <div>{isLoading ? 'Loading... ' : 'We are Ready'}</div>;
-  
+render() {
+  const { isLoading, movies } = this.state;
+  return (
+  <div>
+  {isLoading
+  ? 'Loading...'
+  : movies.map(movie => {
+  console.log(movie);
+  return <Movie />;
+  })}
+  </div>
+  );
   }
-}
+ }
 
 export default App;
