cbbb76e8b0e2a5612ed7a0b22e0f351befd756d2
pickyham <hammieprogrammer@gmail.com>
2020-04-23 21:51:39 +0900

    07-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
+
 
 class App extends React.Component{
 state = {
@@ -22,11 +24,14 @@ this.getMovies();
 render() {
   const { isLoading, movies } = this.state;
   return (
-    <div>
-    {isLoading
-    ? 'Loading...'
-    : movies.map(movie => (
-    
+    <section className="container">
+    {isLoading ? (
+    <div className="loader">
+    <span className="loader__text">Loading...</span>
+    </div>
+    ) : (
+    <div className="movies">
+    {movies.map(movie => (
     <Movie
     key={movie.id}
     id={movie.id}
@@ -34,12 +39,13 @@ render() {
     title={movie.title}
     summary={movie.summary}
     poster={movie.medium_cover_image}
+    genres={movie.genres}
     />
-    
-  ))}
+    ))}
     </div>
-  );
-  }
- }
-
-export default App;
+    )}
+    </section>
+    );
+    }
+   }
+   export default App;
