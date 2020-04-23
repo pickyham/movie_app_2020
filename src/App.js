54b8601d98a812502bc98c9d6bd7516805dee0de
pickyham <hammieprogrammer@gmail.com>
2020-04-23 20:31:40 +0900

    액션 06-4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-
+import axios from 'axios';
 
 class App extends React.Component{
 state = {
   isLoading : true,
+  movies : [],
 };
+getMovies = async() => {
+  const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  console.log(movies.data.data.movies);
+}
 componentDidMount(){
-  setTimeout(()=> {
-    this.setState({
-      isLoading : false
-    });
-  }, 6000)
+this.getMovies();
 }
 render(){
   const {isLoading} = this.state;
-return <div>{isLoading ? 'Loading... ' : 'We are Ready'}</div>;
-}
+  return <div>{isLoading ? 'Loading... ' : 'We are Ready'}</div>;
+  
+  }
 }
 
 export default App;
